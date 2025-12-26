574f38789678a3af40d4e4bf59cbe0f392331f1c
Chaima elomrani <chaimaelomrani6@gmail.com>
2025-12-26 02:33:47 +0100

    added the sessions controller and the method created session using the logic of it from session model

---
+++ controllers/SessionController.php
@@ -0,0 +1,59 @@
+<?php
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../models/Session.php';
+
+class SessionController {
+    // public function getAllSessions(){
+    //     $db = Database::getInstance()->getConnection();
+
+    // }
+
+
+    public function createSession(){
+        if(!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'coach'){
+            $_SESSION['error'] = "only coaches can create sessions.";
+            header('Location: ../views/login.php');
+            exit();
+        }
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $coach_id = $_SESSION['user_id'];
+            $date = $_POST['date'] ?? '';
+            $time = $_POST['time'] ?? '';
+            $duration = $_POST['duration'] ?? '';
+          
+            if(empty($date) || empty($time) || empty($duration)){
+                $_SESSION['error']= "All fields are required.";
+                header('Location: ../views/coach/dashboard.php');
+                exit();
+            }
+        }
+
+        $today = date('Y-m-d');
+        if($date < $today){
+            $_SESSION['error'] = "The date must be in the future.";
+            header('Location: ../views/coach/dashboard.php');
+            exit();
+        }
+
+
+        try{
+            $session = new Session(null, $coach_id,$date , $time, $duration);
+            if($session->createSession($date , $time, $duration)){
+                $_SESSION['success'] = "Session created successfully.";
+                header('Location: ../views/coach/dashboard.php');
+            }else{
+                $_SESSION['error'] = "Failed to create session.";
+                header('Location: ../views/coach/dashboard.php');
+            }
+
+            exit();
+        }catch(Exception $e){
+            $_SESSION['error'] = "An error occurred: " . $e->getMessage();
+            header('Location: ../views/coach/dashboard.php');
+            exit();
+        }
+    }
+
+
+}
