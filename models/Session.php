61322c94519e7d82713323a6ed823dc4855052f3
Chaima elomrani <chaimaelomrani6@gmail.com>
2025-12-26 02:02:01 +0100

    added the create , delete and get all methods in the new created class session

---
+++ models/Session.php
@@ -0,0 +1,67 @@
+<?php
+require_once __DIR__ . '/../config/database.php';
+
+class Session {
+    private $id ;
+    private $coach_id ;
+    private $date ;
+    private $time ;
+    private $duration ;
+    
+
+    public function __construct($id = null, $coach_id, $date, $time, $duration){
+        $this->id = $id ;
+        $this->coach_id = $coach_id ;
+        $this->date = $date ;
+        $this->time = $time ;
+        $this->duration = $duration ;
+    
+    }
+
+    public function getId(){
+        return $this->id ;
+    }
+    public function getCoachId(){
+        return $this->coach_id ;
+    }
+    public function getDate(){
+        return $this->date ;
+    }
+    public function getTime(){
+        return $this->time ;
+    }
+    public function getDuration(){
+        return $this->duration ;
+    }
+
+
+       public function  createSession($date, $time, $duration){
+        $db = Database::getInstance()->getConnection();
+        $stmt = $db->prepare("INSERT INTO sessions (coach_id , date , time , duration , statut)
+        VALUES (:coach_id , :date , :time , :duration , :statut)");
+        return $stmt->execute([ 
+            ':coach_id' => $this->getId(),
+            ':date' => $date,
+            ':time' => $time,
+            ':duration' => $duration,
+            ':statut' => 'disponible'
+        ]);
+    }
+
+    public function deleteSession($session_id){
+        $db = Database::getInstance()->getConnection();
+        $stmt = $db->prepare("DELETE FROM sessions WHERE id = :session_id");
+        return $stmt->execute([
+            ':session_id' => $session_id
+        ]);
+    }
+
+
+    public function getAllSessions(){
+        $db = Database::getInstance()->getConnection();
+        $stmt = $db->prepare("SELECT * FROM sessions");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+}
