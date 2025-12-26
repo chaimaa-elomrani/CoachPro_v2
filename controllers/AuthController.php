0f497cc112af8a025bf4ddb711ea6cb41c9dd3ce
Chaima elomrani <chaimaelomrani6@gmail.com>
2025-12-26 01:42:08 +0100

    added the login function

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -19,6 +19,17 @@ class AuthController
 
 
 
+    public function getUserByEmail($email){
+        $db = Database::getInstance()->getConnection();
+        $stmt =$db->prepare("SELECT * FROM  users WHERE email = :email");
+        $stmt->execute([
+            ':email' => $email
+        ]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+
     public function register(){
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
             $name = $_POST['name'] ?? '';
@@ -63,6 +74,41 @@ class AuthController
             }catch(Exception $e){
                 $_SESSION['error'] = 'Registration failed: ' . $e->getMessage();
                 header('Location: ../views/register.php');
+                exit();
+            }
+        }
+    }
+
+    
+
+    public function login(){
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $email = $_POST['email'] ?? '';
+            $password = $_POST['password'] ?? '';
+
+            if(empty($email) || empty($password)){
+                $_SESSION['error'] = 'Empty fields are not allowed.';
+                header('Location: ../views/login.php');
+                exit();
+            }
+
+            $user = $this->getUserByEmail($email);
+            if($user && password_verify($password , $user['password'])){
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['user_name'] = $user['name'];
+                $_SESSION['user_role'] = $user['role'];
+
+                if($user['role'] === 'coach'){
+                    header('Location: ../views/coach/dashboard.php');
+                }else if ($user['role'] === 'sportif'){
+                    header('Location: ../views/sportif/dashboard.php');
+                }else{
+                    header('Location: ../views/login.php');
+                }
+                exit();
+            }else{
+                $_SESSION['error'] = 'Invalid email or password.';
+                header('Location: ../views/login.php');
                 exit();
             }
         }
